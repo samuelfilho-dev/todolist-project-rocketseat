d3c4b8a106ec84d052aaef3d95c8d88ee15f1100
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-10-12 13:57:45 -0300

    feat: add auth route

--- src/main/java/br/com/rocketseat/todolist/filter/FilterTaskAuth.java
+++ src/main/java/br/com/rocketseat/todolist/filter/FilterTaskAuth.java
@@ -7,7 +7,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -15,37 +15,48 @@ import java.io.IOException;
 import java.util.Base64;
 
 @Component
+@RequiredArgsConstructor
 public class FilterTaskAuth extends OncePerRequestFilter {
 
-    @Autowired
-    private UserModelRepository userModelRepository;
+    private final UserModelRepository userModelRepository;
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
             throws ServletException, IOException {
 
-        // Get Auth User
-        var authorization = request.getHeader("Authorization");
-        var authEncoded = authorization.substring("Basic".length()).trim();
-        var authDecoded = Base64.getDecoder().decode(authEncoded);
-        var authString = new String(authDecoded);
+        var servletPath = request.getServletPath();
 
-        var credentials = authString.split(":");
-        String username = credentials[0];
-        String password = credentials[1];
+        if (servletPath.equals("/tasks/")) {
+            // Get Auth User
+            var authorization = request.getHeader("Authorization");
+            var authEncoded = authorization.substring("Basic".length()).trim();
+            var authDecoded = Base64.getDecoder().decode(authEncoded);
+            var authString = new String(authDecoded);
 
-        // Verify Username
-        UserModel user = this.userModelRepository.findByUsername(username);
+            var credentials = authString.split(":");
+            String username = credentials[0];
+            String password = credentials[1];
 
-        if (user == null)
-            response.sendError(401, "User is not exists");
+            // Verify Username
+            UserModel user = this.userModelRepository.findByUsername(username);
 
-        // Verify Password
-        var verifyPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
+            if (user == null) {
+                response.sendError(401, "User is not exists");
+            } else {
+                // Verify Password
+                var verifyPassword = BCrypt.verifyer().verify(password.toCharArray(), user.getPassword());
 
-        if (verifyPassword.verified)
+                if (verifyPassword.verified) {
+                    filterChain.doFilter(request, response);
+                } else {
+                    response.sendError(401, "User is not exists");
+                }
+            }
+
+        } else {
             filterChain.doFilter(request, response);
+        }
 
-        response.sendError(401, "User is not exists");
+
     }
 }
