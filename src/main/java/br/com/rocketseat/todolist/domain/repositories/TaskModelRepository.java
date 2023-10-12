e4afc2a7b1e0bfab21b719c76605da1baf637a94
samuelfilho-dev <snamuelfilho2011@gmail.com>
2023-10-12 14:32:56 -0300

    fix: get All User Tasks

--- src/main/java/br/com/rocketseat/todolist/domain/repositories/TaskModelRepository.java
+++ src/main/java/br/com/rocketseat/todolist/domain/repositories/TaskModelRepository.java
@@ -4,8 +4,11 @@ import br.com.rocketseat.todolist.domain.models.TaskModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.UUID;
 
 @Repository
 public interface TaskModelRepository extends JpaRepository<TaskModel, UUID> {
+
+    List<TaskModel> findByIdUser(UUID idUser);
 }
